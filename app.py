83f8e5d7bc2853da559660d1c2e272ccc5e1704d
morfeo <aeraso3@gmail.comm>
2022-05-30 21:13:35 -0500

    primer commit

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+
+model=pickle.load(open('antecedentes_hospitalizacion.pkl','rb'))
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world'
+@app.route('/',methods=['POST','GET'])
+def new():
+    return render_template('new.html')
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST','GET'] )
 def predict():
-    return 'Prediciendo'
+    data1=float(request.form['a'])
+    data2=float(request.form['b'])
+    data3=float(request.form['c'])
+    data4=float(request.form['d'])
+    data5=float(request.form['e'])
+    data6=float(request.form['f'])
+    data7=float(request.form['g'])
+    features=np.array([data1,data2,data3,data4,data5,data6,data7])
+    pred = model.predict([features])
+    
+    def statement():
+        if pred == 0:
+            return 'Resultado:- El modelo ha pronosticado que no será hospitalizado pero debe cuidarse.'
+        elif pred == 1:
+            return 'Resultado:- Debe consultar con el médico, el modelo ha predicho que deberá ser hospitalizado.'
+    
+    return render_template('new.html',statement=statement())
 
 
-if __name__ == "__main__":
+if __name__=='__main__':
     app.run()
