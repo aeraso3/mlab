c86b297f1b9d5c9c040977f16bdaa306874dde1b
aeraso3 <aeraso3@gmail.com>
2022-06-01 22:43:28 -0500

    eight commit

--- app.py
+++ app.py
@@ -12,22 +12,22 @@ def new():
 
 @app.route('/predict', methods=['POST','GET'] )
 def predict():
-    data1=float(request.form['a'])
-    data2=float(request.form['b'])
-    data3=float(request.form['c'])
-    data4=float(request.form['d'])
-    data5=float(request.form['e'])
-    data6=float(request.form['f'])
-    data7=float(request.form['g'])
+    data1=int(request.form['a'])
+    data2=int(request.form['b'])
+    data3=int(request.form['c'])
+    data4=int(request.form['d'])
+    data5=int(request.form['e'])
+    data6=int(request.form['f'])
+    data7=int(request.form['g'])
     features=np.array([data1,data2,data3,data4,data5,data6,data7])
     pred = model2.predict([features])
     
     def statement():
         if pred == 0:
-            return 'Resultado: -El modelo ha pronosticado que los síntomas descritos no ponen en riesgo su vida pero debe cuidarse.'
+            return 'Result:- The model has predicted that you will not suffer from any cardic arresst but you should take care of your self.'
         elif pred == 1:
-            return 'Resultado: -Debe consultar con el médico, el modelo ha predicho que sus síntomas ponen en riesgo su vida.'
-    pyt
+            return 'Result:- You should consult with doctor, The model has predicted that you will suffer form cardic arrest.'
+    
     return render_template('new.html',statement=statement())
 
 
